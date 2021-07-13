b3ccfcaed0c1c4cac874e8596e58dc10a4526e7e
Andre Arko <andre@arko.net>
2021-07-13 01:43:13 -0700

    create spotlight then require xattr, rescue errors

--- lib/bundler/mac.rb
+++ lib/bundler/mac.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require_relative "mac/version"
-require_relative "mac/xattr"
 
 module Bundler
   module Mac
@@ -13,11 +12,16 @@ module Bundler
       Bundler.bundle_path.join(".metadata_never_index").write("")
 
       # Tell Time Machine to skip backing up the bundle
+      require_relative "mac/xattr"
       path = Bundler.bundle_path.to_s
       exclude_key = "com.apple.metadata:com_apple_backup_excludeItem"
       exclude_value = "bplist00_\x10\x11com.apple.backupd\b"
       Bundler.ui.debug "[time machine excluded] #{path}"
       Xattr.setxattr(path, exclude_key, exclude_value, exclude_value.size, 0, 0)
+    rescue => e
+      # If something goes wrong, trace in debug mode but otherwise continue.
+      # This isn't critical functionality.
+      Bundler.ui.trace(e)
     end
   end
 end
