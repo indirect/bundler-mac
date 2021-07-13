fe3c3cc1733081f3b9d88d18c29fb783d79e99f4
Andre Arko <andre@arko.net>
2021-07-13 00:48:37 -0700

    do the things and stuff

--- lib/bundler/mac.rb
+++ lib/bundler/mac.rb
@@ -1,11 +1,25 @@
 # frozen_string_literal: true
 
 require_relative "mac/version"
+require_relative "mac/xattr"
 
 module Bundler
   module Mac
-    class Error < StandardError; end
 
-    def self.after_install_all(dependencies); end
+    def self.after_install_all(_)
+      path = Bundler.bundle_path.to_s
+
+      # Don't exclude "system" gems
+      return if Gem.path.include?(path)
+
+      # Skip Spotlight indexing inside the bundle
+      Bundler.root.join(".metadata_never_index").write("")
+
+      # Tell Time Machine to skip backing up the bundle
+      exclude_key = "com.apple.metadata:com_apple_backup_excludeItem"
+      exclude_value = "bplist00_\x10\x11com.apple.backupd\b"
+      Xattr.setxattr(path, exclude_key, exclude_value, exclude_value.size, 0, 0)
+    end
+
   end
 end
---
+++ lib/bundler/mac/xattr.rb
@@ -0,0 +1,12 @@
+require "fiddle"
+require "fiddle/import"
+
+module Bundler
+  module Mac
+    module Xattr
+      extend Fiddle::Importer
+      dlload Fiddle::Handle::DEFAULT
+      extern "int setxattr(const char *, const char *, void *, int, int, int)"
+    end
+  end
+end
