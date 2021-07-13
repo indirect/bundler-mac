84c756be31f7aa21702033482d0cd52a980f280d
Andre Arko <andre@arko.net>
2021-07-12 23:55:57 -0700

    generate a gem

---
+++ lib/bundler/mac.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require_relative "mac/version"
+
+module Bundler
+  module Mac
+    class Error < StandardError; end
+
+    def self.after_install_all(dependencies)
+
+    end
+  end
+end
---
+++ spec/bundler/mac_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Bundler::Mac do
+  it "has a version number" do
+    expect(Bundler::Mac::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
