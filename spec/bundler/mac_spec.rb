ffbfbd8a8fb01bde803214c5d28c1e6127943f59
Andre Arko <andre@arko.net>
2021-07-13 01:36:22 -0700

    test that it works, at least

--- spec/bundler/mac_spec.rb
+++ spec/bundler/mac_spec.rb
@@ -1,11 +1,32 @@
 # frozen_string_literal: true
 
+require "tmpdir"
+require "shellwords"
+
 RSpec.describe Bundler::Mac do
   it "has a version number" do
     expect(Bundler::Mac::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  def xattrs(path)
+    `/usr/bin/xattr #{Shellwords.escape(path)}`
+  end
+
+  def bundle_path
+    Dir.glob(".bundle/ruby/*").first
+  end
+
+  it "sets the Time Machine xattr and spotlight file" do
+    Dir.mktmpdir("spec/tmp") do |dir|
+      Dir.chdir(dir) do
+        File.write "Gemfile", ""
+        Bundler.with_unbundled_env do
+          expect(bundle_path).to eq(nil)
+          `bundle config path .bundle && bundle install`
+          expect(xattrs(bundle_path)).to eq("com.apple.metadata:com_apple_backup_excludeItem\n")
+          expect(Dir.glob("#{bundle_path}/.m*").first).to end_with(".metadata_never_index")
+        end
+      end
+    end
   end
 end
