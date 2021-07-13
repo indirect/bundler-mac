54b712b8b164c38ff58b364454b03d45c64ff29c
Andre Arko <andre@arko.net>
2021-07-13 01:00:23 -0700

    debug log the excluded path

--- lib/bundler/mac.rb
+++ lib/bundler/mac.rb
@@ -18,6 +18,7 @@ module Bundler
       # Tell Time Machine to skip backing up the bundle
       exclude_key = "com.apple.metadata:com_apple_backup_excludeItem"
       exclude_value = "bplist00_\x10\x11com.apple.backupd\b"
+      Bundler.ui.debug "[time machine excluded] #{path}"
       Xattr.setxattr(path, exclude_key, exclude_value, exclude_value.size, 0, 0)
     end
 
